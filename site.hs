0ee6864a2d0804e196940e9fe139af97ebc281b9
Alan Duncan <alan@cocoa-factory.com>
2014-02-15 05:52:16 -0600

    whitespace

--- site.hs
+++ site.hs
@@ -31,6 +31,7 @@ pandocWriteOptions = defaultHakyllWriterOptions
     { writerReferenceLinks = True
     }
 
+
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyllWith config $ do
